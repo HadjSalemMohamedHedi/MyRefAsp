cfb4170fcf6f2342a2eeeba2da6cb9d3eb5ca03a
Mohamed Hedi HADJ SALEM <m.hadjsalem@ekium.eu>
2023-08-14 11:59:23 +0200

    Context

--- Core/Entities/Cours/DB/Cours_C.cs
+++ Core/Entities/Cours/DB/Cours_C.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Core.Entities.Cours.DB
 {
     public partial class Cours_C
     {
+        [Key]
         public int CoursId { get; set; }
         public string Name { get; set; }
     }
--- Core/Entities/Personnes/Models/DB/Personne.cs
+++ Core/Entities/Personnes/Models/DB/Personne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,10 @@ namespace Core.Entities.Personnes.Models.DB
 {
     public partial class Personne
     {
+        [Key]
         public int PersonneId { get; set;}
         public string Name { get; set;}
         public string firstName { get; set;}
-        public DateOnly dateBirth { get; set;}
+        public DateTime dateBirth { get; set;}
     }
 }
